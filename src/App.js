f6d80e76187346b380047fb8a5fed0d9bf17f3c6
shamilkotta <shamilkotta99@gmail.com>
2023-01-11 13:16:06 +0530

    post context

--- src/App.js
+++ src/App.js
@@ -12,16 +12,19 @@ import ViewPost from './Pages/ViewPost';
 import NotFound from './Pages/NotFound';
 
 const initialUser = { email: "", name: "", status: false };
+const initialPost = {};
 export const AuthContext = createContext(initialUser);
+export const PostContext = createContext(initialPost)
 
 
 function App() {
   const auth = getAuth();
   const [user, setUser] = useState(initialUser);
+  const [post, setPost] = useState(initialPost)
 
   useEffect(() => {
     onAuthStateChanged(auth, (currentUser) => {
-      if (currentUser) setUser({ email: currentUser.email, name: currentUser.displayName, status: true });
+      if (currentUser) setUser({ userId: currentUser.uid, email: currentUser.email, name: currentUser.displayName, status: true });
       else setUser(initialUser);
     });
   }, [auth])
@@ -29,18 +32,20 @@ function App() {
 
   return (
     <AuthContext.Provider value={{ user }}>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path='/create' element={<Create />} />
-          <Route path='/login' element={!user.status ? <Login /> : <Navigate to="/" />} />
-          <Route path='/signup' element={!user.status ? <Signup /> : <Navigate to="/" />} />
-          <Route path='/viewpost' element={<ViewPost />} />
-          <Route path='/404' element={<NotFound />} />
-          <Route path='/' element={<Home />} />
-          <Route path='*' element={<Navigate to="/404" replace />} />
-        </Routes>
-      </BrowserRouter>
+      <PostContext.Provider value={{ post, setPost }}>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path='/create' element={user.status ? <Create /> : <Navigate replace to="/login" />} />
+            <Route path='/login' element={!user.status ? <Login /> : <Navigate replace to="/" />} />
+            <Route path='/signup' element={!user.status ? <Signup /> : <Navigate replace to="/" />} />
+            <Route path='/viewpost' element={<ViewPost />} />
+            <Route path='/404' element={<NotFound />} />
+            <Route path='/' element={<Home />} />
+            <Route path='*' element={<Navigate to="/404" replace />} />
+          </Routes>
+        </BrowserRouter>
+      </PostContext.Provider>
     </AuthContext.Provider>
   );
 }
