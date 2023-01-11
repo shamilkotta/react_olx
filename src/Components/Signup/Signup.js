6b037e56edd54e49f3a52199c052847388d65a12
shamilkotta <shamilkotta99@gmail.com>
2023-01-11 09:50:57 +0530

    signup logic

--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -1,58 +1,116 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
 import Logo from '../../olx-logo.png';
 import './Signup.css';
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { addDoc, collection } from "firebase/firestore";
+import { db } from '../../firebase/config';
+import { Link, useNavigate } from 'react-router-dom';
+
+const initialData = {
+  name: "",
+  email: "",
+  phone: "",
+  password: ""
+}
 
 export default function Signup() {
+  const auth = getAuth();
+  const [data, setData] = useState(initialData);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (loading) {
+      setError("");
+    }
+  }, [loading]);
+
+  useEffect(() => {
+    setError("");
+  }, [data]);
+
+
+
+  const handleSubmit = (e) => {
+    setLoading(true);
+    e.preventDefault();
+    createUserWithEmailAndPassword(auth, data.email, data.password)
+      .then(() => updateProfile(auth.currentUser, { displayName: data.name }))
+      .then(() => addDoc(collection(db, "users"), { user: auth.currentUser.uid, email: data.email, phone: data.phone }))
+      .then(() => {
+        setData(initialData);
+        setLoading(false);
+        navigate("/");
+      })
+      .catch((err) => {
+        setLoading(false);
+        setError(err.code === 'auth/email-already-in-use' ? "User already exist" : "Something went wrong");
+      });
+  }
   return (
     <div>
       <div className="signupParentDiv">
-        <img width="200px" align="middle" height="200px" src={Logo}></img>
-        <form>
-          <label htmlFor="fname">Username</label>
+        <img alt='olx' width="200px" align="middle" height="200px" src={Logo}></img>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="name">Username</label>
           <br />
           <input
             className="input"
             type="text"
-            id="fname"
+            id="name"
             name="name"
-            defaultValue="John"
+            value={data.name}
+            onChange={(e) => { setData((prevs) => ({ ...prevs, name: e.target.value })); }}
+            required
           />
           <br />
-          <label htmlFor="fname">Email</label>
+          <label htmlFor="email">Email</label>
           <br />
           <input
             className="input"
             type="email"
-            id="fname"
+            id="email"
             name="email"
-            defaultValue="John"
+            value={data.email}
+            onChange={(e) => { setData((prevs) => ({ ...prevs, email: e.target.value })); }}
+            required
           />
           <br />
-          <label htmlFor="lname">Phone</label>
+          <label htmlFor="phone">Phone</label>
           <br />
           <input
             className="input"
             type="number"
-            id="lname"
+            id="phone"
             name="phone"
-            defaultValue="Doe"
+            value={data.phone}
+            onChange={(e) => { setData((prevs) => ({ ...prevs, phone: e.target.value })); }}
+            required
           />
           <br />
-          <label htmlFor="lname">Password</label>
+          <label htmlFor="password">Password</label>
           <br />
           <input
             className="input"
             type="password"
-            id="lname"
+            id="password"
             name="password"
-            defaultValue="Doe"
+            value={data.password}
+            onChange={(e) => { setData((prevs) => ({ ...prevs, password: e.target.value })); }}
+            required
           />
           <br />
           <br />
-          <button>Signup</button>
+          <p style={{ color: "red" }}>{error}</p>
+          {
+            loading ?
+              <button disabled>Loading...</button>
+              :
+              <button type='submit'>Signup</button>
+          }
         </form>
-        <a>Login</a>
+        <Link to="/login" >Login</Link>
       </div>
     </div>
   );
