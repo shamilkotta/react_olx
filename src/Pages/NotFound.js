a6eb1b15910987942792d01c4c3782279902d72c
shamilkotta <shamilkotta99@gmail.com>
2023-01-11 09:54:06 +0530

    404 page

---
+++ src/Pages/NotFound.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const style = {
+  height: "100vh",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center"
+};
+
+const NotFound = () => {
+  return (
+    <div style={style} >NotFound</div>
+  );
+};
+
+export default NotFound;
