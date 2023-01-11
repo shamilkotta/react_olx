e9a13ba6040d7821d17be1f047396d75a289c9e3
shamilkotta <shamilkotta99@gmail.com>
2023-01-11 13:17:13 +0530

    single post view

--- src/Components/View/View.js
+++ src/Components/View/View.js
@@ -1,26 +1,48 @@
-import React from 'react';
+import { collection, getDocs, query, where } from 'firebase/firestore';
+import React, { useContext, useEffect, useState } from 'react';
+import { PostContext } from '../../App';
+import { db } from '../../firebase/config';
 
 import './View.css';
 function View() {
+
+  const { post } = useContext(PostContext);
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    setData(post);
+    const q = query(collection(db, "users"), where("user", "==", post.userId));
+    let userinfo;
+    getDocs(q)
+      .then((res) => {
+        res.forEach(ele => {
+          userinfo = ele.data();
+        });
+        setData({ ...post, userName: userinfo.name, phone: userinfo.phone });
+      });
+
+  }, [post])
+
+
   return (
     <div className="viewParentDiv">
       <div className="imageShowDiv">
         <img
-          src="../../../Images/R15V3.jpg"
+          src={data.image}
           alt=""
         />
       </div>
       <div className="rightSection">
         <div className="productDetails">
-          <p>&#x20B9; 250000 </p>
-          <span>YAMAHA R15V3</span>
-          <p>Two Wheeler</p>
-          <span>Tue May 04 2021</span>
+          <p>&#x20B9; {250000} </p>
+          <span>{data.name}</span>
+          <p>{data.category}</p>
+          <span>{data.createdAt}</span>
         </div>
         <div className="contactDetails">
           <p>Seller details</p>
-          <p>No name</p>
-          <p>1234567890</p>
+          <p>{data.userName}</p>
+          <p>{data.phone}</p>
         </div>
       </div>
     </div>
