25e269b28d26f77c8afbbe6df7eeb4bd3f0214e1
shamilkotta <shamilkotta99@gmail.com>
2023-01-11 09:51:26 +0530

    firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAPeWCEZWt_6iylzbczSB6Mpz-20GwCFEc",
+  authDomain: "olxclone-6abd7.firebaseapp.com",
+  projectId: "olxclone-6abd7",
+  storageBucket: "olxclone-6abd7.appspot.com",
+  messagingSenderId: "185230786457",
+  appId: "1:185230786457:web:dc9d8cb35c3e3cddf89d08"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
